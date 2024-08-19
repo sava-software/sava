e3f6f982e79864ece377aeafbfcd468e0f456750
jpe7s <jpe7s.salt188@passfwd.com>
2024-08-19 17:32:02 -0500

    move off dynamic version dependencies.

--- core/src/main/java/software/sava/core/programs/DiscriminatorRecord.java
+++ core/src/main/java/software/sava/core/programs/DiscriminatorRecord.java
@@ -19,4 +19,19 @@ record DiscriminatorRecord(byte[] data) implements Discriminator {
   public int length() {
     return data.length;
   }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o instanceof Discriminator discriminator) {
+      return Arrays.equals(this.data, discriminator.data());
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(data);
+  }
 }
