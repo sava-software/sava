98048197b148afc5350dc8f14ac85af2d46034c6
jpe7s <jpe7s.salt188@passfwd.com>
2024-09-03 15:26:17 -0500

    pub key json parser.

--- rpc/src/main/java/software/sava/rpc/json/PublicKeyEncoding.java
+++ rpc/src/main/java/software/sava/rpc/json/PublicKeyEncoding.java
@@ -4,12 +4,23 @@ import software.sava.core.accounts.PublicKey;
 import systems.comodal.jsoniter.CharBufferFunction;
 import systems.comodal.jsoniter.JsonIterator;
 
+import static software.sava.core.accounts.PublicKey.PUBLIC_KEY_LENGTH;
+
 public final class PublicKeyEncoding {
 
   public static CharBufferFunction<PublicKey> PARSE_BASE58_PUBLIC_KEY = PublicKey::fromBase58Encoded;
 
   public static PublicKey parseBase58Encoded(final JsonIterator ji) {
     return ji.applyChars(PARSE_BASE58_PUBLIC_KEY);
+  }
+
+  public static PublicKey parseIntArrayEncoded(final JsonIterator ji) {
+    final byte[] publicKey = new byte[PUBLIC_KEY_LENGTH];
+    int i = 0;
+    for (; ji.readArray(); ++i) {
+      publicKey[i] = (byte) ji.readInt();
+    }
+    return PublicKey.createPubKey(publicKey);
   }
 
   private PublicKeyEncoding() {
