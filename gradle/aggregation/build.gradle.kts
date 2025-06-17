f25adc28c5f36c6604bda2777cfe55b23ae085cb
jpe7s <jpe7s.salt188@passfwd.com>
2025-06-17 09:20:35 -0500

    Use publishToGitHubPackages.

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -6,3 +6,8 @@ dependencies {
   nmcpAggregation(project(":sava-core"))
   nmcpAggregation(project(":sava-rpc"))
 }
+
+tasks.register("publishToGitHubPackages") {
+  group = "publishing"
+  dependsOn(":sava-core:publish", ":sava-rpc:publish")
+}
